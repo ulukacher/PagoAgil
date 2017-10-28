ef7d9f940128301a4cbd1a588d60267f5341dad4
ulukacher <uri.lukacher@gmail.com>
2017-10-28 19:36:46 -0300

    agrego archivos que no agregue en el commit anterior

---
+++ Classes/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba.Classes
+{
+    public class Usuario
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int CantidadIntentosFallidos { get; set; }
+        public long? SucursalId { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
---
+++ Helpers/EncryptHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba.Helpers
+{
+    public class EncryptHelper
+    {
+        public static byte[] SHA256Encrypt(string input)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(input));
+
+                //foreach (Byte b in result)
+                //    Sb.Append(b.ToString("x2"));
+                return result;
+            }
+
+        }
+    }
+}
