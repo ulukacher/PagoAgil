c7213ebd16742bf401ad4ec94e04fc546276ee42
ulukacher <uri.lukacher@gmail.com>
2017-10-30 21:16:18 -0300

    pareceria andar las rendiciones

--- IndexForm.cs
+++ IndexForm.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using PagoAgilFrba.Rendicion;
+using PagoAgilFrba.Rendiciones;
 
 namespace PagoAgilFrba
 {
