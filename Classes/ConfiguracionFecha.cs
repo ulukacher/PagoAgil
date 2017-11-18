a2dd27fff9de18c3e1103865b7771a86e970239a
totojanin2 <totojanin2@hotmail.com>
2017-11-18 14:42:56 -0300

    .

---
+++ Classes/ConfiguracionFecha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace PagoAgilFrba.Classes
+{
+    public static class ConfiguracionFecha
+    {
+        public static DateTime FechaSistema
+        {
+            get
+            {
+                string value = ConfigurationManager.AppSettings["FechaSistema"];
+
+                DateTime fechaSistema = DateTime.Parse(value);
+
+                return fechaSistema;
+            }
+        }
+    }
+}
